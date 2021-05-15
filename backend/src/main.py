199db97954b93e93cd8169f496ea123e7cb76c8f
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-05-15 22:13:16 +0900

    一旦commit

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify
 app = Flask(__name__)
 
 
-@app.route("/", methods=["GET"])
+@app.route("/api/hello", methods=["GET"])
 def hello():
     response = {
         "message": "Hello, World!"
