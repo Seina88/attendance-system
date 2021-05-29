0678967892278b1ada762e7b92f6f4c9b4302e63
Seina Tohyama <tiara818seina@gmail.com>
2021-05-29 16:04:34 +0900

    setting

--- backend/src/config.py
+++ backend/src/config.py
@@ -6,3 +6,14 @@ load_dotenv()
 class Config:
     host = os.getenv("API_HOST") or "localhost"
     port = os.getenv("API_PORT") or 5000
+
+    # SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://{user}:{password}@{host}/{database}?charset=utf8'.format(
+        **{
+            'user': os.getenv('DB_USER', 'root'),
+            'password': os.getenv('DB_PASSWORD'),
+            'host': os.getenv('DB_HOST', 'database'),
+            'database': os.getenv('DB_DATABASE', 'hoge'),
+        })
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = False
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,13 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
 from flask_cors import CORS
-from config import Config
+from src.config import Config
+from src.database import init_db
+from src.apis import hoge
 
 
 def create_app():
     app = Flask(__name__)
     api = Api(app)
     CORS(app)
+    app.config.from_object(Config)
+    init_db(app)
+    api.add_resource(hoge.HogeListAPI, '/hoges')
+    api.add_resource(hoge.HogeAPI, '/hoges/<id>')
     return app, api
 
 
