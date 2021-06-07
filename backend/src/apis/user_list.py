1d974145f5dce47c06b1496ea57cc5bcdbfe5762
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-06-07 18:07:26 +0900

    ちょっと修正

--- backend/src/apis/user_list.py
+++ backend/src/apis/user_list.py
@@ -4,23 +4,34 @@ from ..models.user_model import UserModel, UserSchema
 from ..repositories.user_repository import UserRepository
 from ..repositories.session_repository import SessionRepository
 from ..services.user_service import UserService
+from ..services.session_service import SessionService
 
 
 class UserList(Resource):
     def __init__(self):
         self.user_repository = UserRepository(database)
+        self.session_repository = SessionRepository(database)
         self.user_service = UserService(self.user_repository)
+        self.session_service = SessionService(
+            self.user_repository, self.session_repository)
         self.reqparse = reqparse.RequestParser()
-        self.reqparse.add_argument("nickname", required=True)
-        self.reqparse.add_argument("first_name", required=True)
-        self.reqparse.add_argument("last_name", required=True)
-        self.reqparse.add_argument("email", required=True)
-        self.reqparse.add_argument("password", required=True)
+        self.reqparse.add_argument("Cookie", location="headers")
+        self.reqparse.add_argument("nickname")
+        self.reqparse.add_argument("first_name")
+        self.reqparse.add_argument("last_name")
+        self.reqparse.add_argument("email")
+        self.reqparse.add_argument("password")
         super().__init__()
 
     def get(self):
+        args = self.reqparse.parse_args()
+        cookie = args["Cookie"]
+        if not self.session_service.authenticated(cookie):
+            abort(403, **{"message": "権限がありません。"})
+
         users = self.user_repository.find_all()
-        return {"users": UserSchema(many=True).dump(users)}, 200
+        res = {"users": UserSchema(many=True).dump(users)}
+        return res, 200
 
     def post(self):
         args = self.reqparse.parse_args()
@@ -28,7 +39,7 @@ class UserList(Resource):
                          args.last_name, args.email, args.password)
 
         if self.user_service.exists(user):
-            abort(409, **{"message": "そのメールアドレスはすでに登録されています。"})
+            abort(409, **{"message": "そのユーザーはすでに登録されています。"})
 
         self.user_repository.add(user)
         self.user_repository.commit()
--- backend/src/services/session_service.py
+++ backend/src/services/session_service.py
@@ -1,10 +1,18 @@
+from datetime import datetime
+
+
 class SessionService:
     def __init__(self, user_repository, session_repository):
         self.user_repository = user_repository
         self.session_repository = session_repository
 
     def authenticated(self, cookie):
-        return self.session_repository.find_by_cookie(cookie) is not None
+        session = self.session_repository.find_by_cookie(cookie)
+        if session is None:
+            return False
+        if session.expire_at < datetime.now():
+            return False
+        return True
 
     def find_user_by_cookie(self, cookie):
         session = self.session_repository.find_by_cookie(cookie)
