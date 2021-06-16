4c1ff5ffc2fce159d3f2af45e32e455eab5d227c
Shuntaro Shimizu <ut.s.shimizu@gmail.com>
2021-06-16 22:11:21 +0900

    軽微な修正

--- backend/src/tests/test_case_impl.py
+++ backend/src/tests/test_case_impl.py
@@ -2,13 +2,12 @@ from flask import Flask
 from flask_testing import TestCase
 
 from main import app
+from container import container
 
 from tests.fixtures.seed import seed
 
 from configs.app_config import TestAppConfig
 from configs.database_config import TestDatabaseConfig
-
-from infrastructures.database import db
 
 
 class TestCaseImpl(TestCase):
@@ -19,11 +18,11 @@ class TestCaseImpl(TestCase):
 
     def setUp(self) -> None:
         self.client = self.app.test_client()
-        self.db = db
+        self.db = container.inject("Database")
 
         self.db.create_all()
         self.db.session.commit()
-        seed(db)
+        seed(self.db)
 
     def tearDown(self) -> None:
         self.db.session.remove()
